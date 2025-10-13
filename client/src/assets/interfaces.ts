bb2155d546e073bd134a1bc5177f8ea54c9679dc
zarah <devvick230385@gmail.com>
2025-10-13 16:08:05 +0100

    chore: define game interfaces

---
+++ client/src/assets/interfaces.ts
@@ -0,0 +1,22 @@
+interface Property {
+  name: string;
+  price: number;
+  rent: number[];
+  color: string;
+  position: number;
+  owner: number | null;
+  houses: number;
+  type: "property" | "railroad" | "utility" | "special";
+  housePrice?: number;
+}
+
+interface Player {
+  id: number;
+  name: string;
+  position: number;
+  money: number;
+  properties: number[];
+  color: string;
+  piece: string;
+  isComputer: boolean;
+}
