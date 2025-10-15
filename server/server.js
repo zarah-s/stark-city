2958bde4ab82833392e7b9e79761608df8439613
zarah <devvick230385@gmail.com>
2025-10-15 13:19:45 +0100

    chore: update cors origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
